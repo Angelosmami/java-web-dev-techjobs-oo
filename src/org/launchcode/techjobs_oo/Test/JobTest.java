cb36c0224b4d37bfa553e98a90422fbbd491a0f8
Jennifer <Angelosmami@yahoo.com>
2021-02-02 21:42:01 -0800

    part4 complete

--- src/org/launchcode/techjobs_oo/Test/JobTest.java
+++ src/org/launchcode/techjobs_oo/Test/JobTest.java
@@ -3,22 +3,26 @@ package org.launchcode.techjobs_oo.Test;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
+import org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 
 public class JobTest {
 
     Job job1;
     Job job2;
     Job job3;
+    Job job4;
 
     @Before
     public void setUp() {
          job1 = new Job();
          job2 = new Job();
          job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+         job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
+
 
     @Test
     public void testSettingJobId(){
@@ -34,5 +38,11 @@ public class JobTest {
         assertEquals("Desert", job3.getLocation().getValue());
         assertEquals("Quality control", job3.getPositionType().getValue());
         assertEquals("Persistence", job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void TestForEquality(){
+        assertFalse( job1.equals (job2));
+
     }
 }
