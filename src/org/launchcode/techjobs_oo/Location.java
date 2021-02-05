05dd7836361df995defbc02cfd60bb98af218ab0
Jennifer <Angelosmami@yahoo.com>
2021-02-04 20:13:38 -0800

    added JobField

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -2,20 +2,20 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class Location {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
-
-    public Location(String value){
-        this();
-        this.value = value;
-    }
+public class Location extends JobField {
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public Location() {
+//        id = nextId;
+//        nextId++;
+//    }
+//
+//    public Location(String value){
+//        this();
+//        this.value = value;
+//    }
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
@@ -42,16 +42,16 @@ public class Location {
     }
 
     // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 }
